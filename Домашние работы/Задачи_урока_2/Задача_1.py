19abf309672cfcfaa4443fc678fa8356a9595004
Vladislav <mvd-96@mail.ru>
2023-04-13 21:19:25 +0300

    Решение задач_2

--- Домашние работы/Задачи_урока_2/Задача_1.py
+++ Домашние работы/Задачи_урока_2/Задача_1.py
@@ -1,7 +1,15 @@
-#Напишите программу, которая принимает на вход число N и выдает список факториалов для чисел от 1 до N.
-#пусть N = 4 -> [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+# Напишите программу, которая принимает на вход число N и выдает список факториалов для чисел от 1 до N.
+# пусть N = 4 -> [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+def methodOne():
+    numFact = int(input('Введите число '))
+    count = 1
+    while 1 < numFact:
+     count = count * numFact
+     numFact-=1
+    print(count)
 
-numFact=int(input('Введите число '))
-count = 0
-while count <=numFact:
-    
+def methodTwo():
+    from math import factorial
+    numb = int(input('Введите число '))
+    print(factorial(numb))
+methodOne()
